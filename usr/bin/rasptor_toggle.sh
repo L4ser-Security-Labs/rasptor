90198c574fb93d48d0c208d717ef327f12cd7031
L4ser Security Labs <l4sec@Woles-MacBook-Pro.local>
2025-05-08 23:20:07 +0300

    Initial release

--- usr/bin/rasptor_toggle.sh
+++ usr/bin/rasptor_toggle.sh
@@ -2,8 +2,8 @@
 
 if systemctl is-active --quiet tor; then
   echo "[*] Tor is active. Disabling routing..."
-  /usr/local/bin/disable-tor-routing.sh
+  /usr/bin/rasptor_disable.sh
 else
   echo "[*] Tor is inactive. Enabling routing..."
-  /usr/local/bin/enable-tor-routing.sh
+  /usr/bin/rasptor_enable.sh
 fi
